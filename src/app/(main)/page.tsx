7cebc32551614aef9b8aad51c98691977718eaeb
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-07-10 10:51:07 +0330

    create(schema): create file in validation

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signUpSchema, SignUpSchemaType } from '@/validations/signUpSchema';
+
 export default function SignUp() {
+  
+  const { register, handleSubmit, formState: { errors } } = useForm<SignUpSchemaType>({
+    resolver: zodResolver(signUpSchema)
+  })
+
   return (
     <section className='container flex flex-col-reverse gap-7 md:gap-0 md:flex-row items-center justify-center min-h-screen my-3 xl:my-0'>
       <div>
@@ -12,14 +21,26 @@ export default function SignUp() {
             Sign in to start managing your projects.</p>
           </div>
 
-          <form action="" className='my-7 flex flex-col gap-3'>
+          <form onSubmit={handleSubmit(onSubmit)} action="" className='my-7 flex flex-col gap-3'>
             <div className='flex flex-col justify-center gap-1'> 
               <label htmlFor="">Email</label>
-              <input type="text" placeholder='Example@email.com' className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-xl placeholder:text-[#8897AD]'/>
+              <input {...register("email")}
+               type="text" placeholder='Example@email.com' className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-xl placeholder:text-[#8897AD]'/>
+              {errors.email && (
+                <span className='text-red-500 text-sm'>
+                  {errors.email.message}
+                </span>
+              )}
             </div>
             <div className='flex flex-col justify-center gap-1'> 
               <label htmlFor="">Password</label>
-              <input type="text" placeholder='At least 8 characters' className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-xl placeholder:text-[#8897AD]'/>
+              <input {...register("password")}
+              type="text" placeholder='At least 8 characters' className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-xl placeholder:text-[#8897AD]'/>
+              {errors.password && (
+                <span className='text-red-500 text-sm'>
+                  {errors.password.message}
+                </span>
+              )}
             </div>
             <div className='flex flex-col justify-center items-end gap-3 mt-3'>
               <span className='text-[#1E4AE9] hover:cursor-pointer'>Forgot Password?</span>
