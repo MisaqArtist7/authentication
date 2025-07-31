a188f80540175cdcf9f3a981e3c6f320ed278c0d
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-07-31 13:25:07 +0330

    Add show password button "EyeBtn"

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 
 // Zod schema for form validation
 const signInSchema = z.object({
-  email: z.string().email({ message: "please enter valid email" }),
+  email: z.email({ message: "please enter valid email" }),
   password: z.string()
     .min(8, { message: "password must be at least 8 characters" })
     .max(32, { message: "password must be at most 32 characters" })
@@ -33,6 +33,12 @@ export default function SignUp() {
     console.log(data);
     // You can replace this with actual login logic (API call, etc.)
   };
+  
+  // show current password to user
+  const [showPassword, setShowPassword] = React.useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState)
+  }
 
   return (
     <section className='w-full min-h-screen flex items-center justify-center mb-4'>
@@ -77,12 +83,28 @@ export default function SignUp() {
             {/* Password field */}
             <div className='flex flex-col justify-center gap-1'> 
               <label htmlFor="">Password:</label>
-              <input
-                {...register("password")}
-                type="text"
-                placeholder='Enter your password'
-                className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-lg placeholder:text-[#8897AD]'
-              />
+              <div className='w-full relative'>
+                <input
+                  {...register("password")}
+                  type={`${showPassword ? 'text' : 'password'}`}
+                  placeholder='Enter your password'
+                  className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-lg placeholder:text-[#8897AD] w-full'
+                />
+                {showPassword 
+                ? <span onClick={togglePasswordVisibility} className='absolute flex-row-center right-3 bottom-0 top-0'>
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                  </svg>
+                </span>
+                :
+                  <span onClick={togglePasswordVisibility} className='absolute flex-row-center right-3 bottom-0 top-0'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                  </span>
+                }
+              </div>
               {errors.password && (
                 <span className='text-red-500 text-sm'>{errors.password.message}</span>
               )}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 // Zod schema for sign-up form validation
 const signUpSchema = z.object({
   fullName: z.string().min(3, { message: "Full name must be at least 3 characters" }),
-  email: z.string().email({ message: "Please enter a valid email" }),
+  email: z.email({ message: "Please enter a valid email" }),
   password: z.string()
     .min(8, { message: "Password must be at least 8 characters" })
     .max(32, { message: "Password must be at most 32 characters" })
@@ -40,6 +40,12 @@ export default function SignUp() {
     console.log(data);
     // Here you can send data to backend (e.g., using fetch or axios)
   };
+
+  // show current password to user
+  const [showPassword, setShowPassword] = React.useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState)
+  }
 
   return (
     <section className='w-full min-h-screen flex items-center justify-center mb-4'>
@@ -94,12 +100,28 @@ export default function SignUp() {
             {/* Password */}
             <div className='flex flex-col gap-1'>
               <label htmlFor="">Password:</label>
-              <input
-                {...register("password")}
-                type="password"
-                placeholder='Enter your password'
-                className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-lg placeholder:text-[#8897AD]'
-              />
+              <div className='w-full relative'>
+                <input
+                  {...register("password")}
+                  type={`${showPassword ? 'text' : 'password'}`}
+                  placeholder='Enter your password'
+                  className='bg-[#F7FBFF] border border-[#D4D7E3] py-2 px-3 rounded-lg placeholder:text-[#8897AD] w-full'
+                />
+                {showPassword 
+                ? <span onClick={togglePasswordVisibility} className='absolute flex-row-center right-3 bottom-0 top-0'>
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                  </svg>
+                </span>
+                :
+                  <span onClick={togglePasswordVisibility} className='absolute flex-row-center right-3 bottom-0 top-0'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                  </span>
+                }
+              </div>
               {errors.password && <span className='text-red-500 text-sm'>{errors.password.message}</span>}
             </div>
 
